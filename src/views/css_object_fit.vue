6bc7a1c30a0b670f2b4d28e39c3c0bd9c1e10f00
akicho8 <akicho8@gmail.com>
2018-10-28 22:44:36 +0900

    [chore] css_object_fit

--- src/views/css_object_fit.vue
+++ src/views/css_object_fit.vue
@@ -63,24 +63,25 @@
               span.button.is-static.is-small
                 | 子
             p.control
-              input.input.is-small(type="text" v-model.trim="div1_ws" placeholder="width")
+              input.input.is-small(type="text" v-model.trim="img1_ws" placeholder="width")
             p.control
-              input.input.is-small(type="text" v-model.trim="div1_hs" placeholder="height")
+              input.input.is-small(type="text" v-model.trim="img1_hs" placeholder="height")
 
       .buttons
-        button.button.is-small(@click="mode_fill") 初期値
-        button.button.is-small(@click="mode_cover") トリミング(欠け)
-        button.button.is-small(@click="mode_contain") 比率維持(含む)
+        button.button.is-small(@click="preset_default") 初期値
+        button.button.is-small(@click="preset_trimming") 切り抜き
+        button.button.is-small(@click="preset_contain") 絶対比率維持
+        button.button.is-small(@click="preset_none") 伸縮なし
 
       pre.is-size-7(v-text="css_body")
 
       template(v-if="NODE_ENV !== 'production'")
         .section
           .box
-            div {{div1_style}}
+            div {{img1_style}}
 
     .column
-      img.div1(:style="div1_style" :src="img_files_current" @click="img_files_next")
+      img.img1(:style="img1_style" :src="image_files_current" @click="image_files_next")
 </template>
 
 <script>
@@ -98,21 +99,20 @@ export default {
   data() {
     return {
       real_value_p: false,
-      object_position_p: true,
 
-      object_fit: "cover",
-      div1_w_p: true,
-      div1_h_p: true,
-      div1_w: 3 * 32 * 5,
-      div1_h: 4 * 32 * 5,
-      div1_ws: "",
-      div1_hs: "",
+      object_fit: null,
+      img1_w_p: null,
+      img1_h_p: null,
+      img1_w: null,
+      img1_h: null,
+      img1_ws: null,
+      img1_hs: null,
+      object_position_p: null,
+      object_position_x: null,
+      object_position_y: null,
+      current_image_index: null,
 
-      object_position_x: 36,
-      object_position_y: 50,
-
-      img_files_index: 0,
-      img_files: [
+      image_files: [
         // ISG106132539_TP_V,
         IS107112702_TP_V,
         // I9A5312ISUMI_TP_V,
@@ -122,8 +122,8 @@ export default {
       ],
 
       input_elements: [
-        { key: "div1_w",            name: "横幅", real_name: "img width",         range: { min: 0,    max: 800, step: 1, }, display_key: "div1_w_p", },
-        { key: "div1_h",            name: "縦幅", real_name: "img height",        range: { min: 0,    max: 800, step: 1, }, display_key: "div1_h_p",  },
+        { key: "img1_w",            name: "横幅", real_name: "img width",         range: { min: 0,    max: 800, step: 1, }, display_key: "img1_w_p", },
+        { key: "img1_h",            name: "縦幅", real_name: "img height",        range: { min: 0,    max: 800, step: 1, }, display_key: "img1_h_p",  },
         { key: "object_position_x", name: "基点x", real_name: "object-position x", range: { min: -100, max: 200, step: 1, }, },
         { key: "object_position_y", name: "基点y", real_name: "object-position y", range: { min: -100, max: 200, step: 1, }, },
         {
@@ -142,64 +142,74 @@ export default {
   },
 
   created() {
+    this.preset_default()
   },
 
   watch: {
   },
 
   methods: {
-    mode_fill() {
+    preset_default() {
+      this.current_image_index = 0
       this.object_fit = "fill"
-
-      this.div1_w_p = true
-      this.div1_h_p = true
-      this.div1_w = 480
-      this.div1_h = 640
-      this.div1_ws = ""
-      this.div1_hs = ""
+      this.img1_w_p = true
+      this.img1_h_p = true
+      this.img1_w = 480
+      this.img1_h = 640
+      this.img1_ws = ""
+      this.img1_hs = ""
+      this.object_position_p = true
+      this.object_position_x = 50
+      this.object_position_y = 50
     },
 
-    mode_cover() {
-      this.mode_fill()
+    preset_trimming() {
+      this.preset_default()
+
       this.object_fit = "cover"
+      this.object_position_x = 36
+      this.object_position_y = 50
     },
 
-    mode_contain() {
-      this.mode_fill()
+    preset_contain() {
+      this.preset_default()
       this.object_fit = "contain"
     },
 
-    mode_absolute() {
-      this.object_fit = "absolute"
+    preset_none() {
+      this.preset_default()
+      this.object_fit = "none"
+      this.object_position_x = 32
+      this.object_position_y = 16
     },
 
-    img_files_next() {
-      this.img_files_index += 1
+    image_files_next() {
+      this.current_image_index += 1
     },
   },
 
   computed: {
-    img_files_current() {
-      return this.img_files[this.img_files_index % this.img_files.length]
+    image_files_current() {
+      return this.image_files[this.current_image_index % this.image_files.length]
     },
 
-    div1_style() {
+    img1_style() {
       let hash = {}
 
       if (this.object_fit !== 'fill') {
         hash["object-fit"] = this.object_fit
       }
-      if (this.div1_w_p) {
-        hash["width"] = `${this.div1_w}px`
+      if (this.img1_w_p) {
+        hash["width"] = `${this.img1_w}px`
       }
-      if (this.div1_h_p) {
-        hash["height"] = `${this.div1_h}px`
+      if (this.img1_h_p) {
+        hash["height"] = `${this.img1_h}px`
       }
-      if (this.div1_ws !== "") {
-        hash["width"] = `${this.div1_ws}`
+      if (this.img1_ws !== "") {
+        hash["width"] = `${this.img1_ws}`
       }
-      if (this.div1_hs !== "") {
-        hash["height"] = `${this.div1_hs}`
+      if (this.img1_hs !== "") {
+        hash["height"] = `${this.img1_hs}`
       }
       if (this.object_position_p) {
         hash["object-position"] = `${this.object_position_x}% ${this.object_position_y}%`
@@ -210,18 +220,18 @@ export default {
     css_body() {
       let str = ""
       str += `img\n`
-      if (this.div1_ws !== "") {
-        str += `    width: ${this.div1_ws}\n`
+      if (this.img1_ws !== "") {
+        str += `    width: ${this.img1_ws}\n`
       } else {
-        if (this.div1_w_p) {
-          str += `    width: ${this.div1_w}px\n`
+        if (this.img1_w_p) {
+          str += `    width: ${this.img1_w}px\n`
         }
       }
-      if (this.div1_hs !== "") {
-        str += `    height: ${this.div1_hs}\n`
+      if (this.img1_hs !== "") {
+        str += `    height: ${this.img1_hs}\n`
       } else {
-        if (this.div1_h_p) {
-          str += `    height: ${this.div1_h}px\n`
+        if (this.img1_h_p) {
+          str += `    height: ${this.img1_h}px\n`
         }
       }
       if (this.object_fit !== 'fill') {
@@ -239,7 +249,7 @@ export default {
 <style scoped lang="sass">
 @import "../assets/scss/variables"
 
-.div1
+.img1
   border: 1px dotted $primary
   background: hsla(0, 0, 0, 0.02)
   // object-position: right bottom
