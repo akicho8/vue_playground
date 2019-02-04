132aa100a4f1752d7c223e9d7d4b6d4ec3aca797
akicho8 <akicho8@gmail.com>
2019-02-04 23:13:21 +0900

    [feat] css_font_test

---
+++ src/views/css_font_test.vue
@@ -0,0 +1,83 @@
+<template lang="pug">
+.css_font_test
+  h2.title {{current_title}}
+  hr
+
+  .columns
+    .column
+      article.message.is-primary
+        .message-header
+          | いかもどき
+        .message-body
+          .spla_font
+            | {{sample_text}}
+          a(href="https://aramugi.com/?page_id=807" target="_blank") https://aramugi.com/?page_id=807
+    .column
+      article.message.is-primary
+        link(href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet")
+        .message-header
+          | ニコもじ
+        .message-body
+          .nico_moji_font
+            | {{sample_text}}
+          a(href="https://googlefonts.github.io/japanese/#nicomoji" target="_blank") https://googlefonts.github.io/japanese/#nicomoji
+    .column
+      article.message.is-primary
+        link(href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet")
+        .message-header
+          | はんなり
+        .message-body
+          .hannari_font
+            | {{sample_text}}
+          a(href="https://googlefonts.github.io/japanese/#hannari" target="_blank") https://googlefonts.github.io/japanese/#hannari
+
+</template>
+
+<script>
+import dayjs from "dayjs"
+import form_parts from "./form_parts.vue"
+
+export default {
+  name: "css_font_test",
+  data() {
+    return {
+      sample_text: `
+あいうえお スプラトゥーン 漢字
+0123456789
+０１２３４５６７８９
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ
+ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ
++ ! @ # $ % & ( )
+`
+    }
+  },
+}
+</script>
+
+<style scoped lang="sass">
+@import "../assets/scss/variables"
+
+.message-body
+  word-break: break-all
+
+//////////////////////////////////////////////////////////////////////////////// 「いかもどき」フォントの場所がわかるもの
+@font-face
+  font-family: "SplaFontFace"
+  src: url(../assets/ikamodoki/ikamodoki1_0.ttf)
+
+.spla_font
+  font-family: "SplaFontFace"
+  font-size: 150%
+
+//////////////////////////////////////////////////////////////////////////////// 「ニコもじ」Google Font を使う場合は link を読み込んでこれ
+.nico_moji_font
+  font-family: "Nico Moji"
+  font-size: 150%
+
+//////////////////////////////////////////////////////////////////////////////// 「はんなり」Google Font を使う場合は link を読み込んでこれ
+.hannari_font
+  font-family: "hannari"
+  font-size: 150%
+</style>
