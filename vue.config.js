20f37f2ea20bc43cc5beae5f34df305f25f1aa3a
akicho8 <akicho8@gmail.com>
2019-03-23 18:02:54 +0900

    [fix] Vue.js で template がビルドできない

--- vue.config.js
+++ vue.config.js
@@ -23,4 +23,17 @@ module.exports = {
   //     googleAnalytics: 'UA-12345678-1'
   //   },
   // },
+
+  // Rails5.1でVue.jsで単一ファイルコンポーネントのエラーがでる（You are using the runtime-only build of Vue where the template compiler is not available）
+  // https://qiita.com/magaya0403/items/3fbe9aa20c6a66b76662
+  // Vue CLI 3でsrcパスの変更方法
+  // https://qiita.com/tsuyoz/items/d9e922353ec326dccba3
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+      },
+    },
+  },
 }
